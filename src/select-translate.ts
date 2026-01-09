bb266bc6d818ed4efa1b8ac7f7f866ec3548728e
fnwk <filip.wnek200@gmail.com>
2026-01-09 21:28:38 +0100

    feat: add google translate option

--- src/select-translate.ts
+++ src/select-translate.ts
@@ -1,10 +1,25 @@
-import { getSelectedText, open, showToast, Toast } from "@raycast/api";
+import { getPreferenceValues, getSelectedText, open, showToast, Toast } from "@raycast/api";
+
+interface Preferences {
+  translationService: "raycast" | "google";
+}
 
 export default async function Command() {
   try {
     const selectedText = await getSelectedText();
     const encodedText = encodeURIComponent(selectedText);
-    await open(`raycast://extensions/raycast/translator/translate?fallbackText=${encodedText}`);
+    const preferences = getPreferenceValues<Preferences>();
+
+    let url: string;
+
+    if (preferences.translationService === "google") {
+      // Google Translate extension by gebeto from Raycast Store
+      url = `raycast://extensions/gebeto/translate/translate?fallbackText=${encodedText}`;
+    } else {
+      url = `raycast://extensions/raycast/translator/translate?fallbackText=${encodedText}`;
+    }
+
+    await open(url);
   } catch {
     await showToast({
       style: Toast.Style.Failure,
